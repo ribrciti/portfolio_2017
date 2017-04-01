e464db12f0b11783e15d313bf9d9c1be9fdf44b9
Rick <rbciti@msn.com>
2017-04-01 12:21:46 -0700

    Modified error in seeds file for technologies

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,11 @@ end
     thumb_image: "http://placehold.it/350x200"
     )
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    :name "Technology #{technology}" 
+    )
+end
+
+puts "3 technologies"
