5eaa203ff9882d213530f7c403f2cf7b878958b8
Rick <rbciti@msn.com>
2017-04-15 08:41:44 -0700

    Implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,6 +15,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -58,7 +59,8 @@ class PortfoliosController < ApplicationController
     end
 
     def portfolio_item_params   
-        params.require(:portfolio).permit(:title, :subtitle,:body, :main_image, :thumb_image)
+        params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, 
+                        technologies_attributes: [:name])
     end
 
 end
